0b221a324c7dd7fc97ab531d9c009908cf0dae5c
Jackson Kennedy <jtk5aw@virginia.edu>
2021-04-09 22:58:45 -0400

    Setting up authorization and endpoint test
    One can now authorize the api with their own spotify and
    get their 20 most recently played tracks sent back to them.
    Now I just need to update the front end to only show the buttons
    that the user should currently have access to. Also, I'm ignoring
    refreshing the access token for now and will deal with that later.

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,71 @@
+require('dotenv').config();
+
 const express = require('express');
+const SpotifyWebApi = require('spotify-web-api-node');
+
 const app = express();
-const port = 3000;
+const port = 9000;
+
+const jsonParser = express.json();
+
+
+const cors = require('cors');
+app.use(cors());
+
+var scopes = ['user-read-private', 'user-read-email', 'user-read-recently-played'],
+    redirectUri = 'http://localhost:3000/',
+    clientId = process.env.SPOTIFY_API_CLIENT_ID,
+    secretId = process.env.SPOTIFY_API_SECRET_ID,
+    state = 'some-state-of-my-choice',
+    showDialog = true,
+    responseType = 'code';
+
+var spotifyApi = new SpotifyWebApi({
+    redirectUri: redirectUri,
+    clientId: clientId,
+    clientSecret: secretId
+});
 
 app.get('/', (req, res) => {
     res.send('Hello world!');
+});
+
+app.post('/spotify-authorize', (req, res) => {
+    var authorizeURL = spotifyApi.createAuthorizeURL(
+        scopes,
+        state,
+        showDialog,
+        responseType
+    );
+
+    res.send({spotifyURL: authorizeURL});
+    
+});
+
+app.post('/spotify-access-token/', jsonParser, (req, res) => {
+
+    spotifyApi.authorizationCodeGrant(req.body.access_token)
+        .then((data) => {
+            spotifyApi.setAccessToken(data.body['access_token'])
+            spotifyApi.setRefreshToken(data.body['refresh_token']);
+
+            // For now sends nothing back but need to make it send email to verify login
+        }, 
+        (err) => {
+            console.log('Something.went wrong!', err);
+        });
+
+});
+
+app.get('/recently-played', (req, res) => {
+    spotifyApi.getMyRecentlyPlayedTracks({
+        limit: 20
+    }).then((data) => {
+        res.send(data.body);
+    }).catch((err) => {
+        console.log('Something went wrong!', err);
+        res.send(err);
+    })
 });
 
 app.listen(port, () => {
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,97 @@
+import React, { 
+  useEffect 
+} from 'react';
+import {
+  useLocation
+} from "react-router-dom";
+import Button from 'react-bootstrap/Button'
 import logo from './logo.svg';
 import './css/App.css';
 
-function App() {
+export default function App() {
+  return (
+    <div>
+      <SpotifyAuthorizationTest />
+    </div>
+  )
+}
+
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
+}
+
+function SpotifyAuthorizationTest() {
+  
+  var query = useQuery();
+  var recentlyPlayed = 'http://localhost:9000/recently-played';
+  var beginAuthorize = 'http://localhost:9000/spotify-authorize';
+  var completeAuthorize = 'http://localhost:9000/spotify-access-token';
+  var afterAuthorize = 'http://localhost:3000/';
+
+  useEffect(() => {
+    if(query.has('code')) {
+      let postBody = {
+        'redirectURL': afterAuthorize,
+        'access_token': query.get('code'),
+      };
+
+      fetch(completeAuthorize, { 
+        'method': 'POST', 
+        'headers': {
+          'Content-Type': 'application/json'
+        },
+        'redirect': 'follow',
+        'body': JSON.stringify(postBody)
+      })
+      .then(response => response.json())
+      .then(data => {
+        console.log('Success:', data);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+    }
+  }, [query, completeAuthorize, afterAuthorize])
+
+  const handleAuthorizeClick = () => {
+
+    fetch(beginAuthorize, { method: 'POST' })
+      .then(response => response.json())
+      .then(data => {
+        if(data.spotifyURL) {
+          // I very much dislike doing this. It feels wrong
+          // But at the moment I can't seem to find another way
+          window.location.href = data.spotifyURL;
+        } 
+      })
+      .catch((err) => {
+        console.log(err + " url: " + beginAuthorize);
+      })
+
+  }
+
+  const handleRecentlyPlayedClick = () => {
+    fetch(recentlyPlayed)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log('There was an error fetching recently played: ' + err);
+      })
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button style={{backgroundColor: "#61dafb"}} size="lg" onClick={handleAuthorizeClick}>
+          Sign Into Spotify
+        </Button>
+        <Button style={{backgroundColor: '#61dafb'}} size='lg' onClick={handleRecentlyPlayedClick}>
+          Fetch Recently Played
+        </Button>
       </header>
     </div>
   );
-}
-
-export default App;
+};
